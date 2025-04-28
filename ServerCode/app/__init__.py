4ea2f2ece0cfdf4751eb4cc826a1bafe22e90c95
FongWongZong <fanhuazeng2004@gmail.com>
2025-04-28 16:53:23 +0800

    database initialisation

--- ServerCode/app/__init__.py
+++ ServerCode/app/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask     
+from flask import Flask
+from flash_migrate import Migrate
+from flash_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)  
 
 if __name__=='__main__': 
---
+++ ServerCode/app/config.py
@@ -0,0 +1,4 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
