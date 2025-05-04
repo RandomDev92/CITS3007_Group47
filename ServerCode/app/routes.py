277985522d7f1065f7a2710e3f12727ea66c4d49
aid-in <aidin.bronson.fenwick@gmail.com>
2025-05-04 19:09:35 +0800

    Added QuestionStatPage queries

--- ServerCode/app/routes.py
+++ ServerCode/app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template,  request, redirect, flash
 from app import app
-from app.models import User, Question, db
+from app.models import User, Question, db, Submission
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from flask_login import current_user
@@ -30,13 +30,35 @@ def SearchPage():
 def UserPage():
     return render_template("UserPage.html", current_user=current_user)
 
-@app.route('/QuestionDescription')
+@app.route('/QuestionDescription/<int:question_id>')
 def QuestionDescriptionPage():
+    question_id = request.args.get('id', type=int)
     return render_template("QuestionDescription.html")
 
 @app.route('/QuestionStat')
 def QuestionStatPage():
-    return render_template("QuestionStat.html")
+    question_id = request.args.get('id', type=int)
+    submission = Submission.query.filter_by(
+        user_id=current_user.id,
+        question_id=question_id
+    ).order_by(Submission.timestamp.desc()).first()
+    if submission:
+        user_score = {
+            'time_taken': submission.runtime_sec,
+            'tests_ran': submission.tests_run,
+            'code_length': submission.lines_of_code,
+            'passed': submission.passed
+        }
+    else:
+        user_score = {
+            'time_taken': "N/A",
+            'tests_ran': "N/A",
+            'code_length': "N/A",
+            'passed': "N/A"
+        }
+    question = Question.query.get_or_404(question_id)
+    return render_template("QuestionResults.html", question=question, user_score=user_score)
+
 
 @app.route('/QuestionAnswer')
 def QuestionAnswer():
