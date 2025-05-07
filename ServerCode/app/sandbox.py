436794386964bb98daa3c8e85c0eccc4349296c3
RandomDev92 <billmrayner2@gmail.com>
2025-05-08 01:02:27 +0800

    regex resolve bug fix

--- ServerCode/app/sandbox.py
+++ ServerCode/app/sandbox.py
@@ -104,8 +104,10 @@ def testCode(stringCode, stringTest):
     
     if type(testingDict) != type(dict()):
         return "Unable to Create Testing."
-    
-    funcName = re.search(r'def (.*?)\(', stringCode).group(1)
+    try: 
+        funcName = re.search(r'def (.*?)\(', stringCode).group(1)
+    except:
+        return "Unable to Resolve Function Name."
     if funcName == None:
         return "Unable to Resolve Function Name."
     
